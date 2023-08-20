339c68d2266348270115d642159c49668c96027a
saksham <sakshampanpaliya5553@gmail.com>
2023-08-20 18:24:38 +0530

    Added ignore.txt and .gitignore

--- factorial.cpp
+++ factorial.cpp
@@ -1,8 +1,21 @@
 #include "functions.h"
+#include<iostream>
+using namespace std;
 
 int factorial(int n){
-    if(n!=1){
-	return(n * factorial(n-1));
+
+    if(n<0){
+
+        cout<<"Factorial not defined for negative numbers."<<endl;
+        cout<<"Please enter a valid positive number"<<endl;
+
+    }else{
+
+        if(n!=1){
+            return(n * factorial(n-1));
+        }
+        else return 1;
+
     }
-    else return 1;
+
 }
