0491f7e0f2223f862dd6fb54e729e8286e6b2ffa
saksham <sakshampanpaliya5553@gmail.com>
2023-08-21 15:25:59 +0530

    Reseting files

--- factorial.cpp
+++ factorial.cpp
@@ -7,18 +7,10 @@ using namespace std;
 
 int factorial(int n){
 
-    if(n<0){
-
-        cout<<"Factorial not defined for negative numbers."<<endl;
-        cout<<"Please enter a valid positive number"<<endl;
-
-    }else{
 
         if(n!=1){
             return(n * factorial(n-1));
         }
         else return 1;
-
-    }
 
 }
