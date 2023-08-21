ddf5e8a32459b0a3d60001ec6fbf029783f4c8dc
saksham <sakshampanpaliya5553@gmail.com>
2023-08-21 15:32:27 +0530

    Added condition for negative in testing

--- factorial.cpp
+++ factorial.cpp
@@ -7,10 +7,18 @@ using namespace std;
 
 int factorial(int n){
 
+    if(n<0){
+        return -1;
+    }else{
 
         if(n!=1){
             return(n * factorial(n-1));
         }
         else return 1;
+
+    }
+
+
+        
 
 }
