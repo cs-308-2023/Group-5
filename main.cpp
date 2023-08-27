eb09f68559e2d440b2fb25f2b0ef927b4ed08f9a
saksham <sakshampanpaliya5553@gmail.com>
2023-08-27 22:20:56 +0530

    final changes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(){
     cout<< endl;
     int x=56,y=98;
     cout<<"The GCD of x and y is: "<<gcd_non_rec(x,y)<< endl;
+    cout<<"The recursive GCD of x and y is: "<<gcd_rec(x,y)<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
     return 0;
 }
