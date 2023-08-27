8c9d9bf6cfb5670614a17d0bd884abf44bff5d17
saksham <sakshampanpaliya5553@gmail.com>
2023-08-27 22:14:39 +0530

    merged

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main(){
     print_hello();
     cout<< endl;
+    int x=56,y=98;
+    cout<<"The GCD of x and y is: "<<gcd_non_rec(x,y)<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
     return 0;
 }
